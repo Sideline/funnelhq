3b9ec78fd57f0f4cd6491c0e746a3683d9c489c3
Owain Lewis <owain@owainlewis.com>
2012-02-23 23:39:32 +0000

    deploying to Heroku at 2012-02-23 23:39:32 +0000

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -9,8 +9,7 @@ class Upload
     :storage => :s3,
       :s3_credentials => {
         :access_key_id => ENV['S3_KEY'],
-        :secret_access_key => ENV['S3_SECRET']
-      }
+        :secret_access_key => ENV['S3_SECRET'] }
       :bucket => ENV['S3_BUCKET'],
       :path => ":attachment/:id/:filename"
     
