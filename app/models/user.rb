8c0574cc0d28d9f1b6d75f1745e9c4082f4fe2ee
Owain Lewis <owain@owainlewis.com>
2012-02-24 22:41:24 +0000

    more formatting ocd

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,10 @@ class User
      
   validates_presence_of :first_name, :last_name
   
-  validates :email, presence: true, uniqueness: true, format: { with: /^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }
+  validates :email, 
+            presence: true, 
+            uniqueness: true, 
+            format: { with: /^[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }
   
   ## associations ##
   
