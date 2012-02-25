224e00bc2f579fc35ea7bad1f00bf42aad655aaf
Owain Lewis <owain@owainlewis.com>
2012-02-25 21:54:06 +0000

    deploying to Heroku at 2012-02-25 21:54:06 +0000

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -7,9 +7,12 @@ class Upload
   
   has_mongoid_attached_file :file,
     :storage => :s3,
-      :s3_credentials => "#{Rails.root}/config/s3.yml",
+      :s3_credentials => {
+        :access_key_id => ENV['S3_KEY'],
+        :secret_access_key => ENV['S3_SECRET']
+      },
       :bucket => ENV['S3_BUCKET'],
-      :path => ":attachment/:id/:filename"
+      :path => ":attachment/#{self.user.id}/:id/:filename"
     
   ## Validation ##
   
